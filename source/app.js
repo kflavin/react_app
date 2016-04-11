9dcb115c353a724c8c02480f0ce9f9f5ee77f9d7
Kyle <Kyle.Flavin@gmail.com>
2016-04-10 22:14:55 -0700

    Introduces basic Kanban app.  Layout only.

--- source/app.js
+++ source/app.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard';
 
-class Hello extends Component {
-    render() {
-        var place ="everybody";
-        return (
-            <h1>Hello {place}</h1>
-            );
-    }
-}
+let cardsList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        description: "I should read the whole book",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+];
 
-// Parent Component
-class GroceryList extends Component {
-    render() {
-        return (
-                <ul>
-                    <ListItem quantity="1" name="bread" />
-                    <ListItem quantity="6" name="Eggs" />
-                    <ListItem quantity="2" name="Milk" />
-                </ul>
-               );
-    }
-}
 
-// Child Component
-class ListItem extends Component {
-    render() {
-        return (
-                <li>
-                    {this.props.quantity}x {this.props.name}
-                </li>
-               );
-    }
-}
-
-
-//React.render(<Hello />, document.getElementById('root'));
-React.render(<GroceryList />, document.getElementById('root'));
+React.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
